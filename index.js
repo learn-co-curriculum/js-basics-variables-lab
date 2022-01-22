e6bf8dc360040a5df0d8b6e171bdee4cb16c7eed
Micaella Colombo <micaelc@g.clemson.edu>
2022-01-22 18:55:54 -0500

    commit tree 81dcf97c5417f4e5a78176e1daf8e244a55694b7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+//companyName
+    //1) is set as Scuber
+const companyName = "Scuber";
+    //2) is defined as a const
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
+
